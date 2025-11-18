3996e63783b95a104bbb89080fee572f6eabf8c8
Luan B Almada <lba.letras@gmail.com>
2025-11-18 15:54:12 -0300

    terminando C(W)RUD de produtos e funcionários

--- app/models/funcionarios_model.py
+++ app/models/funcionarios_model.py
@@ -9,7 +9,7 @@ class FuncionariosModel:
         self.conn = sqlite3.connect(self.db)
         self.cursor = self.conn.cursor()
 
-    # CREATE    
+    # CREATE TABLE    
     def create_tables(self):
         self.connect()
         self.cursor.execute(
@@ -26,7 +26,7 @@ class FuncionariosModel:
         )
         
     # WRITE (INSERT)
-    def insert_funcionario(self, user_name, senha_login, nome, cargo, adm):
+    def inserir_funcionario(self, user_name, senha_login, nome, cargo, adm):
         self.connect()        
         
         self.cursor.execute(
@@ -49,19 +49,76 @@ class FuncionariosModel:
     def listar_funcionarios(self):
         self.connect()
         self.cursor.execute("""SELECT * FROM funcionario""")
-        produtos = self.cursor.fetchall()
-        print(produtos)
+        funcionarios = self.cursor.fetchall()
         self.conn.commit()
         self.conn.close()  
-    
+        return funcionarios
 
     # READ ONE (SELECT)
+    def selecionar_funcionario(self, funcionario_id):
+        self.connect()
+        
+        self.cursor.execute(
+            """
+                SELECT * FROM funcionario WHERE id = ?
+            """, (funcionario_id)
+        )
+        funcionario_selecionado = self.cursor.fetchone()
+        self.conn.commit()
+        self.conn.close()
+        return funcionario_selecionado       
     
     # UPDATE
+    def atualizar_funcionario(self, funcionario_id, user_name, senha_login, nome, cargo, adm):
+        self.connect()
+        
+        self.cursor.execute(
+            """
+                UPDATE funcionario SET 
+                    user_name = ?,
+                    senha_login = ?,
+                    nome = ?,
+                    cargo = ?,
+                    adm = ?
+                WHERE id = ?
+            """, (user_name, senha_login, nome, cargo, adm, funcionario_id)
+        )
+        self.conn.commit()
+        self.conn.close()
     
     # DELETE
-    
+    def excluir_funcionario(self, funcionario_id):
+        self.connect()
+        
+        self.cursor.execute(
+            """
+                DELETE FROM funcionario WHERE id = ?
+            """, (funcionario_id,)
+        )
+
+        self.conn.commit()
+        self.conn.close()
+
+
 if __name__ == "__main__":
-    database = FuncionariosModel()
-    database.insert_funcionario("@adm", "ADMINIT", "adm0", "adm", True)
-    database.listar_funcionarios()
+    func_db = FuncionariosModel()
+    print("__\n\n")
+
+    # func_db.inserir_funcionario("@mikael", "555", "Mikael", "Vendedor", False)
+    # func_db.inserir_funcionario("@mariana", "555", "Mariana", "Florista", False)
+    # print(func_db.listar_funcionarios())
+    # print(func_db.selecionar_funcionario("1"))
+    
+    # for i in range(1, 6):
+    #     print(func_db.selecionar_funcionario(str(i)))
+    # # print("\n>>>>> UPDATE <<<<<")
+    # # func_db.atualizar_funcionario(3, "@joseph", "555", "Joseph", "Florista", False)
+    # # for i in range(1, 6):
+    #     print(func_db.selecionar_funcionario(str(i)))
+
+    # for i in range(1, 6):
+    #     print(func_db.selecionar_funcionario(str(i)))
+    # print("\n>>>>> DELETE <<<<<")
+    # func_db.excluir_funcionario(5)
+    # for i in range(1, 6):
+    #     print(func_db.selecionar_funcionario(str(i)))
--- app/models/produtos_model.py
+++ app/models/produtos_model.py
@@ -8,8 +8,8 @@ class ProdutosModel:
     def connect(self):
         self.conn = sqlite3.connect(self.db)
         self.cursor = self.conn.cursor()
-    
-    # CREATE    
+    # C(W)RUD
+    # CREATE TABLE (produto e lote)
     def create_tables(self):
         self.connect()
         
@@ -42,8 +42,8 @@ class ProdutosModel:
         self.conn.commit()
         self.conn.close()
     
-    # WRITE (INSERT)
-    def insert_produto(self, nome, qtd_estoque):
+    # WRITE (INSERT) - produto
+    def inserir_produto(self, nome, qtd_estoque):
         self.connect()        
         
         self.cursor.execute(
@@ -55,8 +55,28 @@ class ProdutosModel:
         
         self.conn.commit()
         self.conn.close()
+
+    # WRITE (INSERT) - lote
+    def inserir_lote(self, produto_id: int, qtd_lote: int, fabricacao:str, validade:str, registro:str, responsavel:str):
+        self.connect()        
+        
+        self.cursor.execute(
+            """
+                INSERT INTO lote (produto_id, 
+                                     qtd_lote, 
+                                     fabricacao,
+                                     validade,
+                                     registro,
+                                     responsavel) 
+                                     VALUES (?, ?, ?, ?, ?, ?)  
+            """,
+            (produto_id, qtd_lote, fabricacao, validade, registro, responsavel)
+        )
+        
+        self.conn.commit()
+        self.conn.close()
     
-    # READ ALL (LISTAR)
+    # READ ALL (SELECT) - produto
     def listar_produtos(self):
         self.connect()
         
@@ -66,26 +86,159 @@ class ProdutosModel:
             """
         )
         produtos = self.cursor.fetchall()
-        print(produtos)
         self.conn.commit()
-        self.conn.close()    
+        self.conn.close()
+        return produtos    
     
-    # READ ONE (SELECT) Incompleto
-    # def select_produto(self, produto_id):
-    #     self.connect()
+    # READ ALL (SELECT) - lote
+    def listar_lotes(self):
+        self.connect()
         
-    #     self.cursor.execute(
-    #         """
-    #             SELECT * FROM produto WHERE id = ?
-    #         """
-    #     )
-    #     produtos = self.cursor.fetchall()
-    #     print(produtos)
-    #     self.conn.commit()
-    #     self.conn.close()        
+        self.cursor.execute(
+            """
+                SELECT * FROM lote
+            """
+        )
+        lotes = self.cursor.fetchall()
+        self.conn.commit()
+        self.conn.close()
+        return lotes    
+    
+    # READ ONE (SELECT) - produto
+    def selecionar_produto(self, produto_id):
+        self.connect()
+        
+        self.cursor.execute(
+            """
+                SELECT * FROM produto WHERE id = ?
+            """, (produto_id)
+        )
+        produto_selecionado = self.cursor.fetchone()
+        self.conn.commit()
+        self.conn.close()
+        return produto_selecionado        
+    
+    # READ ONE (SELECT) - lote
+    def selecionar_lote(self, lote_id):
+        self.connect()
+        
+        self.cursor.execute(
+            """
+                SELECT * FROM lote WHERE id = ?
+            """, (lote_id)
+        )
+        lote_selecionado = self.cursor.fetchone()
+        self.conn.commit()
+        self.conn.close()
+        return lote_selecionado        
+    
+    # UPDATE - produto
+    def atualizar_produto(self, produto_id, novo_nome, nova_qtd_estoque):
+        self.connect()
+        
+        self.cursor.execute(
+            """
+                UPDATE produto SET 
+                    nome = ?,
+                    qtd_estoque = ?
+                WHERE id = ?
+            """, (novo_nome, nova_qtd_estoque, produto_id)
+        )
+        self.conn.commit()
+        self.conn.close()
+
+    # UPDATE - lote
+    def atualizar_lote(self, lote_id:int, produto_id: int, qtd_lote: int, fabricacao:str, validade:str, registro:str, responsavel:str):
+        self.connect()
+        
+        self.cursor.execute(
+            """
+                UPDATE lote SET 
+                    produto_id = ?,
+                    qtd_lote = ?,
+                    fabricacao = ?,
+                    validade = ?,
+                    registro = ?,
+                    responsavel = ?
+                WHERE id = ?
+            """, (produto_id, qtd_lote, fabricacao, validade, registro, responsavel, lote_id)
+        )
+        self.conn.commit()
+        self.conn.close()
+
+    # DELETE - produto
+    def excluir_produto(self, produto_id):
+        self.connect()
+        
+        self.cursor.execute(
+            """
+                DELETE FROM produto WHERE id = ?
+            """, (produto_id,)
+        )
+
+        self.conn.commit()
+        self.conn.close()
+
+    # DELETE - lote
+    def excluir_lote(self, lote_id):
+        self.connect()
+        
+        self.cursor.execute(
+            """
+                DELETE FROM lote WHERE id = ?
+            """, (lote_id,)
+        )
+
+        self.conn.commit()
+        self.conn.close()
+
     
 if __name__ == "__main__":
     database = ProdutosModel()
-    # database.insert_produto("Balde", 10)
-    database.listar_produtos()
-        
+    print("__\n\n")
+
+    # TESTES - CRUD DE PRODUTO
+
+    # database.inserir_produto("Vaso", 10)
+    # database.inserir_produto("Rosa branca", 10)
+
+    # print("LISTA DE PRODUTOS")
+    # print(database.listar_produtos())    
+    # print("PRODUTO 3")
+    # print("\n>>>>> UPDATE <<<<<")
+    # database.atualizar_produto(3, "Rosa Amarela", 42)
+    # print("PRODUTO 3")
+    # print(database.selecionar_produto("3"))
+    
+    # print(database.selecionar_produto("1"))
+    # print("\n >>>> DELETE <<<<")
+    # database.excluir_produto("1")
+    # print(database.selecionar_produto("1"))
+    
+    # database.excluir_produto("5")   
+    # print(database.listar_produtos())    
+    # for i in range(9, 19):
+    #     indice_em_str = str(i)
+    # print(database.listar_produtos())    
+
+
+    # TESTES - CRUD DE LOTE
+
+    # database.inserir_lote(5, 10, "2025-10-15", "2025-20-25", "2025-10-16", "Marina")
+    # database.inserir_lote(5, 10, "2025-10-15", "2025-20-25", "2025-10-16", "Anna")
+    # # print("\nLOTE 3")
+    # print(database.selecionar_lote("3"))
+    # # print("\n>>>>> UPDATE <<<<<")
+    # database.atualizar_lote(4, 3, 69, "2025-10-17", "2025-10-27", "2025-10-20", "Giullyannah")
+    # print("\nLOTE 3")
+    # print(database.selecionar_lote("3"))
+    # print(database.listar_lotes())
+
+    # for i in range(9):
+    #     print(database.selecionar_lote(str(i)))
+    # print("\n>>>> DELETE <<<<\n")
+    # database.excluir_lote("5")
+    # for i in range(9):
+    #     print(database.selecionar_lote(str(i)))
+    
+    
